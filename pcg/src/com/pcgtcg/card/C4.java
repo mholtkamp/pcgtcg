4ae69fc0f2d8b5901fd0056eb82a4f5fa3cf3cec
mholtkamp <mholt012@gmail.com>
2015-04-03 18:46:06 -0700

    Changed 4's passive effect so that it has +4 power in defense mode.

--- pcg/src/com/pcgtcg/card/C4.java
+++ pcg/src/com/pcgtcg/card/C4.java
@@ -16,7 +16,7 @@ public class C4 extends Card {
 		power = 4;
 		tributeValue = 1;
 		activeDescriptor = "";
-		passiveDescriptor = "Gains 4 power when the sum of the \nnatural powers of the enemy field are a multiple of 4.";
+		passiveDescriptor = "Gains 4 power when in defense position.";
 	}
 
 	public void activate()
@@ -30,31 +30,7 @@ public class C4 extends Card {
 	}
 	
 	public int getPower()
-	{
-	    int total = 0;
-	    Field efield;
-	    if (isOwn)
-	        efield = pcgtcg.game.efield;
-	    else
-	        efield = pcgtcg.game.field;
-	    
-	    for (int i = 0; i < efield.getSize(); i++)
-	    {
-	        total += efield.getCard(i).getNaturalPower();
-	    }
-	    
-	    return (total%4 == 0) ? (power + 4 + powerModifier) : (power + powerModifier);
-	}
-	
-	public void render(SpriteBatch batch)
-	{
-	    super.render(batch);
-//## Uncomment for a visual cue of 4's effect
-//	    if (getPower() == 8)
-//	    {
-//    	    batch.setColor(0.7f, 0.5f, 1.0f, 0.5f);
-//    	    batch.draw(whiteTex, box.x, box.y, box.width, box.height);
-//    	    batch.setColor(1f, 1f, 1f, 1f);
-//	    }
+	{   
+	    return attackPosition ? (power + powerModifier) : (power + powerModifier + 4);
 	}
 }
